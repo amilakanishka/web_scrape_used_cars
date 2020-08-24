eb920942c5b5d2d7de4ebe47217d03868308fa38
amilakanishka <amilakanishka@gmail.com>
2020-08-24 14:09:58 +1000

    add price extract

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def index():
     if os.path.isfile('export.csv'):
         df = pd.read_csv('export.csv', index_col= False)
         df.index = np.arange(1, len(df)+1)
-        table_html = df.to_html()
+        table_html = df.to_html( escape= False)
+        table_html = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><div class="table-responsive-sm">' + table_html + '</div>'
     return render_template('index.html', table_html = table_html)
 
 @app.route('/scrape')
--- scrape_cars.py
+++ scrape_cars.py
@@ -25,15 +25,15 @@ def get_car_url_list():
 
 
     for page in range(1,page_count+1):
-        if (page == 2):
-            break;
+        # if page ==  2:
+        #     break
         url = base_url + url_part + str(page)
         browser.visit(url)
         div_list = soup.find_all('div',class_='vehicle-cardstyles__InfoWrap-sc-1bxv5iu-1 bEIKmT')
         for div in div_list:
             car_list.append(base_url +div.find('a')['href'])
 
-        time.sleep(0.25)
+        time.sleep(0.5)
 
     return car_list
 
@@ -46,22 +46,89 @@ def insert(df, row):
         df.loc[insert_loc + 1] = row
 
 def extract_data(soup, df):
-    make = soup.find('h1').text 
-    model = soup.find('p',class_='sc-hZSUBg ggtemb').text.split(' ')[1]
-    reg_year = soup.find('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT').find_all('p')[1].text
-    meleage = soup.find_all('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')[1].find_all('p')[1].text
-    fuel_type = soup.find_all('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')[2].find_all('p')[1].text
-    transmission = soup.find_all('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')[3].find_all('p')[1].text
-    seats = soup.find_all('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')[4].find_all('p')[1].text
-    engine  = soup.find_all('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')[5].find_all('p')[1].text
-    body_type = soup.find("div", {"data-test-id" : "Body type"}).find('dd').text.split(' ')[-1]
-    doors = soup.find("div", {"data-test-id" : "Body type"}).find('dd').text.split(' ')[0]
-    Gearbox = soup.find("div", {"data-test-id" : "Gearbox"}).find('dd').text.split(' ')[0][:-1]
-    Registration = soup.find("div", {"data-test-id" : "Registration number"}).find('dd').text
-    number_keys = soup.find("div", {"data-test-id" : "Number of keys"}).find('dd').text
-    ext_color = soup.find("div", {"data-test-id" : "Exterior colour"}).find('dd').text
-    drive_type = soup.find("div", {"data-test-id" : "Drive type"}).find('dd').text
-    prev_owners = soup.find("div", {"data-test-id" : "Previous owners"}).find('dd').text
+
+    v = soup.find('h1')
+    make = ''
+    if(v):
+        make = v.text
+
+    v = soup.find('p',class_='sc-hZSUBg ggtemb')
+    model = ''
+    if(v):
+        model = v.text.split(' ')[1]
+
+    v = soup.find('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')
+    reg_year = ''
+    if(v):
+        reg_year = v.find_all('p')[1].text
+
+    v = soup.find_all('li',class_='key-features-and-iconsstyles__KeyFeature-sc-13a1tsx-2 inYPYT')
+    meleage = ''
+    fuel_type = '' 
+    transmission = ''  
+    seats = '' 
+    engine = ''
+    if(v):
+        meleage = v[1].find_all('p')[1].text
+        fuel_type = v[2].find_all('p')[1].text
+        transmission = v[3].find_all('p')[1].text
+        seats = v[4].find_all('p')[1].text
+        engine = v[5].find_all('p')[1].text
+    
+    v = soup.find("div", {"data-test-id" : "summary-vehicle-history"})
+
+    last_srv_date = ''
+    last_srv_mile = ''
+    if(v):
+        hist_list = v.find_all('h3')
+        if(hist_list):
+            for hist in hist_list:                
+                if hist.text == 'Last service':
+                    p_list = hist.find_parent('div').find_all('p')
+                    if len(p_list) == 1:
+                        last_srv_date = hist.find_parent('div').find_all('p')[0].text
+                    else:   
+                        last_srv_date = hist.find_parent('div').find_all('p')[0].text
+                        last_srv_mile = hist.find_parent('div').find_all('p')[1].text
+
+    v = soup.find("div", {"data-test-id" : "Body type"})
+    body_type = ''
+    doors = ''    
+    if(v):
+        body_type = v.find('dd').text.split(' ')[-1]  
+        doors = v.find('dd').text.split(' ')[0]
+
+    v = soup.find("div", {"data-test-id" : "Gearbox"})
+    Gearbox = ''
+    if(v):
+        Gearbox = v.find('dd').text.split(' ')[0][:-1]
+
+    v = soup.find("div", {"data-test-id" : "Registration number"})
+    Registration = ''
+
+    if(v):
+        Registration = v.find('dd').text
+
+    v = soup.find("div", {"data-test-id" : "Number of keys"})
+    number_keys = ''
+    if(v):
+        number_keys = v.find('dd').text
+
+    v = soup.find("div", {"data-test-id" : "Exterior colour"})
+    ext_color = ''
+    if(v):
+        ext_color = v.find('dd').text
+
+    v = soup.find("div", {"data-test-id" : "Drive type"})
+    drive_type = ''
+    if(v):
+        drive_type = v.find('dd').text
+
+    v = soup.find("div", {"data-test-id" : "Previous owners"})
+    prev_owners = ''
+    if(v):
+        prev_owners = v.find('dd').text
+
     car_ext_features_list = soup.find(id = 'exterior-features').find_all('li')
     car_ext_list = ''
     for feature in car_ext_features_list:
@@ -72,24 +139,68 @@ def extract_data(soup, df):
     for feature in car_int_features_list:
         car_int_list = f"{car_int_list} '{feature.text}'"
 
-    top_speed = soup.find("div", {"data-test-id" : "Top speed"}).find('dd').text
-    acceleration = soup.find("div", {"data-test-id" : "Acceleration (0-62 mph)"}).find('dd').text
-    engine_power = soup.find("div", {"data-test-id" : "Engine power"}).find('dd').text
-    length = soup.find("div", {"data-test-id" : "Length"}).find('dd').text
-    fuel_cap = soup.find("div", {"data-test-id" : "Fuel tank capacity"}).find('dd').text
-    boot_cap = soup.find("div", {"data-test-id" : "Boot space (seats-up)"}).find('dd').text 
-    insert(df,[make,model,reg_year,meleage,fuel_type,transmission,seats,engine,body_type,doors,Gearbox,Registration,number_keys,ext_color,drive_type,prev_owners,car_ext_list,car_int_list,top_speed,acceleration,engine_power,length,fuel_cap,boot_cap])
+    v = soup.find("div", {"data-test-id" : "Top speed"})
+    top_speed = ''
+    if(v):
+        top_speed = v.find('dd').text
+
+    v = soup.find("div", {"data-test-id" : "Acceleration (0-62 mph)"})
+    acceleration = ''
+    if(v):
+        acceleration = v.find('dd').text
+
+    v = soup.find("div", {"data-test-id" : "Engine power"})
+    engine_power = ''
+    if(v):
+        engine_power = v.find('dd').text        
+
+    v = soup.find("div", {"data-test-id" : "Length"})
+    length = ''
+    if(v):
+        length = v.find('dd').text 
+
+    v = soup.find("div", {"data-test-id" : "Fuel tank capacity"})
+    fuel_cap = ''
+    if(v):
+        fuel_cap = v.find('dd').text 
+
+    v = soup.find("div", {"data-test-id" : "Boot space (seats-up)"})
+    boot_cap = ''
+    if(v):
+        boot_cap = v.find('dd').text 
+      
+    v = soup.find("img", {"data-test-id" : "default-hero-image"}) 
+    
+    image = '' 
+    if(v):
+        image = f"<img src='{v['src']}' width='100' height='80'>" 
+
+    v = soup.find("p", class_='pricingstyles__PriceValue-sc-10yqv22-3')
+    price = ''
+    if(v):
+        price = v.text   
+
+    insert(df,[image,price,make,model,reg_year,meleage,fuel_type,transmission,seats,engine,body_type,doors,     \
+        Gearbox,Registration,number_keys,ext_color,drive_type,prev_owners,car_ext_list,car_int_list,\
+        top_speed,acceleration,engine_power,length,fuel_cap,boot_cap,last_srv_date,last_srv_mile])
 
 
 def scrape():
-    df = pd.DataFrame(columns=['make','model','reg_year','meleage','fuel_type','transmission','seats','engine','body_type','doors','Gearbox','Registration','number_keys','ext_color','drive_type','prev_owners','car_ext_list','car_int_list','top_speed','acceleration','engine_power','length','fuel_cap','boot_cap'])
+    df = pd.DataFrame(columns=['image','price','make','model','reg_year','meleage','fuel_type','transmission','seats', \
+        'engine','body_type','doors','Gearbox','Registration','number_keys','ext_color','drive_type',  \
+        'prev_owners','car_ext_list','car_int_list','top_speed','acceleration','engine_power','length',\
+        'fuel_cap','boot_cap','last_srv_date','last_srv_mile'])
     url_list = get_car_url_list()
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
-    for url in url_list:
-        browser.visit(url)
+    for url in url_list:        
+        try:
+            browser.visit(url)
+        except:
+            continue
+
         soup = BeautifulSoup(browser.html,'html.parser')
-        time.sleep(0.25)
+        time.sleep(1)
         extract_data(soup, df)  
     browser.quit()    
     df.to_csv('export.csv',index = False)
